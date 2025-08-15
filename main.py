30f421469a17e02bd5b87bcf3b83c29e139698d9
Saurabh <srbhatia10@gmail.com>
2025-08-15 13:42:58 +0200

    add a ticking clock to slow down the game loop to 60fps

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0  # delta-time
     while True:  # game loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
         pygame.display.flip()  # refresh the screen
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
